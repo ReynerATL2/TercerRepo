e1f07ab18dc7b782f339c1134c95d5b3fafd7f03
RoyEiner <roy.cerpa.salas@gmail.com>
2025-09-07 15:01:25 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Roy Cerpa",                             # Tu nombre
+    author_email="roy_cs020692@hotmail.com",        # Tu correo electrónico
+    url="https://github.com/ReynerATL2/TercerRepo", # URL del proyecto
+)
